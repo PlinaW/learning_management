533616b561394cb0bf68711e83786e15b6672e07
PlinaW <plinski.wojciech@gmail.com>
2024-03-15 21:50:57 +0100

    Admin auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, skip: [:registration]
   devise_for :users
   root "courses#index"
   resources :courses
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
 
 end
