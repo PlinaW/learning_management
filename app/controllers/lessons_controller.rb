4fc788f1f8ca7416b2dbbfda4a3b41b973042998
PlinaW <110122241+PlinaW@users.noreply.github.com>
2024-03-25 19:52:15 +0100

    Premium content (#11)
    * Premium content unlock
    
    * Block access to lessons by lesson controller for non premium users
    
    * Repair some layout styling

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,12 @@
 class LessonsController < ApplicationController
   before_action :set_lesson, only: %i[ show update ]
   before_action :set_course
+  before_action :check_paid
 
   def show
     @completed_lessons = current_user.lesson_users.where(completed: true).pluck(:lesson_id)
     @course = @lesson.course
+    @paid_for_course = current_user.course_users.where(course: @course).exists?
   end
 
   def update
@@ -24,10 +26,18 @@ class LessonsController < ApplicationController
       @course = Course.find(params[:course_id])
     end
     
-
-
     # Use callbacks to share common setup or constraints between actions.
     def set_lesson
       @lesson = Lesson.find(params[:id])
     end
+    def check_paid
+      if @lesson.paid && !current_user.course_users.where(course_id: params[:course_id]).exists?
+        if @lesson.previous_lesson
+          redirect_to course_lesson_path(@course, @lesson.previous_lesson), notice: "You must purchase the full course to access the next lesson"
+        else
+          redirect_to course_path(@course), notice: "You must purchase the full course to access the next lesson"
+        end
+      end
+    end
+
 end
