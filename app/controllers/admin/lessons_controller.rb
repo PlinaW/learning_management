bc46a0f39dbfed957d7931d56c3217b90169b1a2
PlinaW <110122241+PlinaW@users.noreply.github.com>
2024-03-26 21:37:49 +0100

    Admin lesson creation (#17)
    * Add new action to AdminLessonController and update form and new view
    
    * Add new action to the AdminLessonController

--- app/controllers/admin/lessons_controller.rb
+++ app/controllers/admin/lessons_controller.rb
@@ -9,6 +9,18 @@ class Admin::LessonsController < AdminController
     
   end
   
+  def new
+    @admin_lesson = @admin_course.lessons.new    
+  end
+
+  def create
+    @admin_lesson = @admin_course.lessons.new(lesson_params)
+    if @admin_lesson.save
+      redirect_to admin_course_lessons_path(@admin_course)
+    else
+      render :new
+    end
+  end
 
   def move
     position = params[:position]
@@ -27,6 +39,11 @@ class Admin::LessonsController < AdminController
   
 
   private
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :description, :video, :paid, :position)
+  end
+  
 
   def set_course
     @admin_course = Course.find(params[:course_id])
