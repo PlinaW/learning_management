94b5448a190e52c304e2225eacf0987c8e8eabdf
PlinaW <110122241+PlinaW@users.noreply.github.com>
2024-03-26 20:26:43 +0100

    Admin lesson sortable (#16)
    * Add lesson_controller.js
    
    * Pin stimulus-sortable
    
    * Configure lesson_controller.js
    
    * Add act_as_list gem, set it and repair show view for admin lessons

--- app/controllers/admin/lessons_controller.rb
+++ app/controllers/admin/lessons_controller.rb
@@ -1,12 +1,40 @@
 class Admin::LessonsController < AdminController
   before_action :set_course
+  before_action :set_admin_lesson, only: [:move, :show]
   def index
     @admin_lessons = @admin_course.lessons.order(:position)
   end
+
+  def show
+    
+  end
+  
+
+  def move
+    position = params[:position]
+    if position == 0
+      @admin_lesson.move_to_top
+    elsif position == @admin_course.lessons.count - 1
+      @admin_lesson.move_to_bottom
+    else
+      @admin_lesson.instert_at(position + 1)
+    end
+
+    @admin_lesson.save!
+
+    render json: { message: "success" }
+  end
+  
 
   private
 
   def set_course
     @admin_course = Course.find(params[:course_id])
   end
+
+  def set_admin_lesson
+    @admin_lesson = @admin_course.lessons.find(params[:id])    
+  end
+  
+
 end
